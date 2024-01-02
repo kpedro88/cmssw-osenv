8e8c57434d39f1a26d60aff67e94e9738084a653
Kevin Pedro <kpedro88@gmail.com>
2024-01-02 05:28:37 -0600

    add single and multi command tests

--- tests/run.sh
+++ tests/run.sh
@@ -4,6 +4,12 @@
 function die { echo $1: status $2 ;  exit $2; }
 
 function tests() {
+	# test single command
+	TEST=0
+	EXPECTED=""
+	RESULT=$(./cmssw-env --cmsos cc7 -B $PWD --pwd $PWD -- echo)
+	if [ "$RESULT" != "$EXPECTED" ]; then die "Test $TEST: wrong output $RESULT (should be $EXPECTED)" 1; fi
+
 	# test argument quoting
 	TEST=1
 	EXPECTED="hello world"
@@ -15,6 +21,17 @@ function tests() {
 	EXPECTED=3
 	RESULT=$(./cmssw-env --cmsos cc7 -B $PWD --pwd $PWD -- ./testArgs.sh 1 "2 3" 4)
 	if [ "$RESULT" -ne "$EXPECTED" ]; then die "Test $TEST: incorrect number of arguments $RESULT (should be $EXPECTED)" 1; fi
+
+	# test argument quoting with script, fully quoted
+	TEST=2b
+	RESULT=$(./cmssw-env --cmsos cc7 -B $PWD --pwd $PWD -- './testArgs.sh 1 "2 3" 4')
+	if [ "$RESULT" -ne "$EXPECTED" ]; then die "Test $TEST: incorrect number of arguments $RESULT (should be $EXPECTED)" 1; fi
+
+	# test multi-command case
+	TEST=3
+	EXPECTED=$(echo foo; echo bar)
+	RESULT=$(./cmssw-env --cmsos cc7 -B $PWD --pwd $PWD -- "echo foo; echo bar")
+	if [ "$RESULT" != "$EXPECTED" ]; then die "Test $TEST: wrong output $RESULT (should be $EXPECTED)" 1; fi
 }
 
 # common setup
