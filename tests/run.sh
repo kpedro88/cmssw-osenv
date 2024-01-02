2fca2c2d38ce43056a246a7d51059be6f94ac573
Kevin Pedro <kpedro88@gmail.com>
2024-01-02 04:12:33 -0600

    add tests for --command-to-run

--- tests/run.sh
+++ tests/run.sh
@@ -5,15 +5,23 @@ function die { echo $1: status $2 ;  exit $2; }
 
 function tests() {
 	# test argument quoting
-	TEST=1
+	TEST=1a
 	EXPECTED="hello world"
 	RESULT=$(./cmssw-env --cmsos cc7 -B $PWD --pwd $PWD -- echo "$EXPECTED")
 	if [ "$RESULT" != "$EXPECTED" ]; then die "Test $TEST: wrong output $RESULT (should be $EXPECTED)" 1; fi
 
+	TEST=1b
+	RESULT=$(./cmssw-env --cmsos cc7 -B $PWD --pwd $PWD --command-to-run 'echo "'"$EXPECTED"'"')
+	if [ "$RESULT" != "$EXPECTED" ]; then die "Test $TEST: wrong output $RESULT (should be $EXPECTED)" 1; fi
+
 	# test argument quoting with script
-	TEST=2
+	TEST=2a
 	EXPECTED=3
 	RESULT=$(./cmssw-env --cmsos cc7 -B $PWD --pwd $PWD -- ./testArgs.sh 1 "2 3" 4)
+	if [ "$RESULT" -ne "$EXPECTED" ]; then die "Test $TEST: incorrect number of arguments $RESULT (should be $EXPECTED)" 1; fi
+
+	TEST=2b
+	RESULT=$(./cmssw-env --cmsos cc7 -B $PWD --pwd $PWD --command-to-run './testArgs.sh 1 "2 3" 4')
 	if [ "$RESULT" -ne "$EXPECTED" ]; then die "Test $TEST: incorrect number of arguments $RESULT (should be $EXPECTED)" 1; fi
 }
 
